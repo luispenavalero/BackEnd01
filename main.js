3187bd8c56e6e110655f8c037ebfc3facfd53797
Luis Pena <luismiguelpvalero@gmail.com>
2023-01-25 01:24:33 -0300

    commit02

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-let catalogo = [ ]
-
 // Contruimos el producto como objeto
 class Item {
+
+    static id = 0
+
     constructor(title, description, price, thumbnail, code, stock) {
         this.title = title;
         this.description = description;
@@ -9,6 +10,7 @@ class Item {
         this.thumbnail = thumbnail;
         this.code = code;
         this.stock = stock;
+        this.id = ++Item.id
     }
 }
 
@@ -23,18 +25,32 @@ class ProductManager {
     }
 
     addProdutc = (title, description, price, thumbnail, code, stock) => {
-        let newItem = new Item(title, description, price, thumbnail, code, stock);
-
-        this.product.push(newItem);
+        if (this.product.some(prod => prod.code === code)) {
+            console.log("Ya este producto existe")          
+        } else {
+            let newItem = new Item (title, description, price, thumbnail, code, stock);
+            this.product.push(newItem)
+            console.log("Item agregado")
+        }
     }
+
+    getElementById = (id) => {
+        console.log(id)
+    }
+
 }
+
 
 // Probamos de introducir data en el Array
 let test =  new ProductManager()
 test.addProdutc("jabón", "Utencilio de limpieza", 50, "img.jpg", 2355, 22);
 test.addProdutc("Vino", "Bebida alcohólica", 200, "img2.jpg", 853, 11);
-test.addProdutc("Vino", "Bebida alcohólica", 200, "img2.jpg", 853, 11);
-test.addProdutc("jabón", "Utencilio de limpieza", 50, "img.jpg", 2355, 22);
+test.addProdutc("Jamón", "Charcutería", 50, "img3.jpg", 2655, 12);
+test.addProdutc("Aceite", "Adereso", 85 , "img4.jpg", 3255, 16);
+test.addProdutc("Aceitunas", "Charcutería", 60, "img5.jpg", 2655, 23);
+
+
+
 
 
 
